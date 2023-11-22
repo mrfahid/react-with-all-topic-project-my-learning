361a40bd4dcca1e55503cebcecfcb61c9ed6ea6e
Shah Fahid <shahfahidbaloch@gmail.com>
2023-11-22 07:05:35 -0800

    set bg Colors

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("#6680f5");
+
+  return (
+    <>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ background: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+              <button
+                onClick={() => setColor("black")}
+                type="button"
+                className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+              >
+                Button text
+              </button>
+              <button
+                onClick={() => setColor("orange")}
+                type="button"
+                className="rounded-md bg-yellow-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-yellow-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
+              >
+                Orange Button
+              </button>
+              <button
+                onClick={() => setColor("red")}
+                type="button"
+                className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+              >
+                Red Button
+              </button>
+              <button
+                onClick={() => setColor("green")}
+                type="button"
+                className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+              >
+                Green Button
+              </button>
+              <button
+                onClick={() => setColor("yellow")}
+                type="button"
+                className="rounded-md bg-yellow-300 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-yellow-400/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-300"
+              >
+                Olive Button
+              </button>
+              <button
+                onClick={() => setColor("hotpink")}
+                type="button"
+                className="rounded-md bg-pink-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-400"
+              >
+                Pink Button
+              </button>
+              <button
+                onClick={() => setColor("purple")}
+                type="button"
+                className="rounded-md bg-purple-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
+              >
+                Purple Button
+              </button>
+              <button
+                onClick={() => setColor("blue")}
+                type="button"
+                className="rounded-md bg-blue-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-600/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+              >
+                Blue Button
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
