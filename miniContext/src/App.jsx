fa4a4eb27177ecc22eb7c9b0cd857326b16e7d89
Shah Fahid <shahfahidbaloch@gmail.com>
2023-11-30 06:26:24 -0800

    mini context project

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,15 @@
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./context/UserContextProvider";
+import './App.css'
+function App() {
+  return (
+    <UserContextProvider>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+
+export default App;
